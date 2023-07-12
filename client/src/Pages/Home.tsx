1c84ea9a75e374bd300112036c69dcfe6209d4bf
Ieva Vyšniauskaitė <vietorame@gmail.com>
2023-07-12 17:48:18 +0300

    added a few comments in Home.tsx

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -20,15 +20,19 @@ const Home = () => {
   const itemsPerPage = 5;
   const formContainerRef = useRef(null);
 
+  //handlina, ar rodys add user formą
   const handleShowForm = (value: boolean) => {
     setShowForm(value);
   };
 
+  //pafetchina data iš API
   useEffect(() => {
     const fetchData = async () => {
       try {
         const userData = await fetchUserData();
+        //settina data
         setData(userData);
+        //nustato users masyvo ilgį, kad žinotų, kaip apskaičiuot, kiek bus psl paginatione
         setTotalItems(userData.length);
       } catch (error) {
         console.error('Error:', error);
@@ -38,13 +42,16 @@ const Home = () => {
     fetchData();
   }, []);
 
+  //nustato, kuris page parinktas
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
+  //editina userį. Paima ID ir updatina userį su funkcija iš API folderio
   const handleEditUser = async (userId: string, updatedUser: IUser) => {
     try {
       await updateUser(userId, updatedUser);
+      //paupdatina turimą data šitam puslapy, kad iškart parodytų pasikeitimus
       const updatedData = await fetchUserData();
       setData(updatedData);
     } catch (error) {
@@ -52,25 +59,35 @@ const Home = () => {
     }
   };
 
+  //deletina userį irgi su delete requestu iš API, kaip ir PUT 51 eilutėj
   const handleDeleteUser = async (userId: string) => {
     try {
       await deleteUser(userId);
+      //po to kai deletina, vel paupdatina
       const updatedData = await fetchUserData();
       setData(updatedData);
+      //sitas true aditaro modalą su žinute, kad ištrinta sėkmingai
       setDeletedUser(true);
+      //vėl per naują nustato, kiek userių yra paginationui
+      setTotalItems(updatedData.length);
     } catch (error) {
       console.error('Error updating user:', error);
     }
   };
 
+  //uždaro trynimo modalą. reikėjo padaryt atskirą funkciją,
+  //nes buttono propsas onClick ima voidinę funckiją kaip type (IButton interface Button componente),
+  //tai tiesiog useState typescriptui nepatiko
   const closeDeletedUserModal = () => {
     setDeletedUser(false);
   };
 
   return (
     <>
+      {/* headeris handlina showform, ar rodys add user formą paspaudus ant mygtuko */}
       <Header setShowForm={handleShowForm} />
       <StyledPage>
+        {/* Jeigu show form yra true, tai rodo modalą su forma */}
         {showForm && (
           <Modal>
             <div ref={formContainerRef}>
@@ -78,7 +95,7 @@ const Home = () => {
             </div>
           </Modal>
         )}
-
+        {/* Čia paprasčiau, čia buttonas iškart kreipiasi į funckiją šitam faile ir ten su useState settina į false, kas uždaro modalą */}
         {deletedUser && (
           <Modal>
             Vartotojas ištrintas sėkmingai
@@ -91,6 +108,13 @@ const Home = () => {
         )}
         <StyledTableContainer>
           <TableHeading></TableHeading>
+          {/* čia paginationas slicina data. (currentPage - 1) * itemsPerPage, paima
+          visus userius iki esamo puslapio, pvz., jeigu esam 4 puslapy, o vienam
+          puslapy rodo 5 userius, tai ( 4 - 1 ) * 5 = 15, tai reiskia, kad
+          numeta 15 useriu nuo pradzios ir rodo nuo 15 user'io (skaiciuojant nuo 0) ir tada
+          currentPage * itemsPerPage būtų 4 * 5 = 20, tai rodo item'us nuo 15
+          iki 19
+          */}
           <TableRow
             data={data.slice(
               (currentPage - 1) * itemsPerPage,
